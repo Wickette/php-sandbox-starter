61c7899eecb8ff6b174fcf3cfa0f42f3156ee226
Wickette <sc.aurora92@gmail.com>
2026-06-12 15:43:20 -0400

    switch statements overview

--- 03-control-structures/07-switch-statements/index.php
+++ 03-control-structures/07-switch-statements/index.php
@@ -1,3 +1,53 @@
+<!-- /*
+switch statements are a control structure that allows you to execute different code based on the value of a variable. They are often used as an alternative to if-else statements when you have multiple conditions to check.
+The syntax for a switch statement is as follows:
+
+switch (variable) {
+    case value1:
+        // code to execute if variable == value1
+        break;
+    case value2:
+        // code to execute if variable == value2
+        break;
+    ...
+    default:
+        // code to execute if variable does not match any case
+}
+*/ -->
+<?php $dayOfWeek = date('l'); // Get the current day of the week
+
+switch($dayOfWeek){
+case "Monday":
+  $message = "Monday blues!";
+  $color = "blue";
+  break;
+  case "Tuesday":
+  $message = "At least it's not Monday!";
+  $color = "green";
+  break;
+  case "Wednesday":
+  $message = "Hump day!";
+  $color = "magenta";
+  break;
+  case "Thursday":
+  $message = "Thirsty Thursday!";
+  $color = "orange";
+  break;
+  case "Friday":
+  $message = "TGIF!";
+  $color = "red";
+  break;
+  case "Saturday":
+  $message = "Have a nice weekend!";
+  $color = "purple";
+  break;
+  case "Sunday":
+  $message = "Have a nice weekend!";
+  $color = "purple";
+  break;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,7 +61,7 @@
   <style>
     body {
       font-family: 'Roboto', sans-serif;
-      background-color: blue;
+      background-color: <?= $color ?>;
       color: white;
       display: flex;
       justify-content: center;
@@ -22,7 +72,7 @@
 </head>
 
 <body>
-  <h1>Monday blues!</h1>
+  <h1><?= $message ?></h1>
 </body>
 
 </html>
